791067f3e0d68ffa65fd47a57a702efaa04f74cb
PapaGede <naqhouamy@gmail.com>
2019-07-18 11:16:55 +0000

    Advanced Python Course

---
+++ advancedpython.py
@@ -0,0 +1,43 @@
+# def is_even(number):
+#     if number % 2==0:
+#         return "true"
+#     else:
+#         return "false"
+
+# number=int(input("Please enter a number: \n"))
+# print(is_even(number))
+
+# numb=[1,56,234,87,4,76,24,69,90,135]
+
+# for i in range (0,10):
+#     if numb[i]%2==0:
+#         print (numb[i])
+
+# names=["sam", "john", "james"]
+# print(list(map(len,names)))
+
+
+
+# def is_even(x):
+#     if x %2 ==0:
+#         return True
+# def not_even(y):
+#     return not(is_even(y))
+
+# numb=[1,56,234,87,4,76,24,69,90,135]
+# print(list(filter(not_even,numb)))
+
+# number=0
+# numbers = [12, 54, 33, 67, 24, 89, 11, 24, 47]
+# [number for number in numbers if number % 2==0]
+# print(number)
+
+# sentence = "the quick brown fox jumps over the lazy dog"
+# words = sentence.split()
+# print(words)
+
+# words = ["hello", "my", "name", "is", "Sam"]
+# print(map(len, words))
+
+names = ["sam", "john", "james"]
+print(list(map(len, names)))
